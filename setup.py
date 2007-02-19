39f27e7038154ed5c3b78a6a4ba42d10f09a7273
Ross Light <ross@zombiezen.com>
2007-02-19 21:17:04 +0000

    Changed version to 0.1.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if sys.version < '2.4.0':
     sys.exit(1)
 
 setup(name="pymage",
-      version='0.1.1',
+      version='0.1.2',
       description="Pygame helper package",
       author="Ross Light",
       author_email='rlight2@gmail.com',
